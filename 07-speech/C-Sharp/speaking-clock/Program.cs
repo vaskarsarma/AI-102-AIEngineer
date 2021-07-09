17236b50d317a8a83877e405a31b7e634bdf6cba
Student <vaskarsarma@gmail.com>
2021-07-09 03:18:59 -0700

    Speech Service

--- 07-speech/C-Sharp/speaking-clock/Program.cs
+++ 07-speech/C-Sharp/speaking-clock/Program.cs
@@ -3,7 +3,9 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
 // Import namespaces
-
+// Import namespaces
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 
 namespace speaking_clock
 {
@@ -21,7 +23,9 @@ namespace speaking_clock
                 string cogSvcRegion = configuration["CognitiveServiceRegion"];
 
                 // Configure speech service
-
+// Configure speech service
+speechConfig = SpeechConfig.FromSubscription(cogSvcKey, cogSvcRegion);
+Console.WriteLine("Ready to use speech service in " + speechConfig.Region);
 
                 // Get spoken input
                 string command = "";
@@ -43,10 +47,29 @@ namespace speaking_clock
             string command = "";
             
             // Configure speech recognition
-
+// Configure speech recognition
+using AudioConfig audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+using SpeechRecognizer speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+Console.WriteLine("Speak now...");
 
             // Process speech input
-
+// Process speech input
+SpeechRecognitionResult speech = await speechRecognizer.RecognizeOnceAsync();
+if (speech.Reason == ResultReason.RecognizedSpeech)
+{
+    command = speech.Text;
+    Console.WriteLine(command);
+}
+else
+{
+    Console.WriteLine(speech.Reason);
+    if (speech.Reason == ResultReason.Canceled)
+    {
+        var cancellation = CancellationDetails.FromResult(speech);
+        Console.WriteLine(cancellation.Reason);
+        Console.WriteLine(cancellation.ErrorDetails);
+    }
+}
 
             // Return the command
             return command;
@@ -58,10 +81,34 @@ namespace speaking_clock
             string responseText = "The time is " + now.Hour.ToString() + ":" + now.Minute.ToString("D2");
                         
             // Configure speech synthesis
-
+// Configure speech synthesis
+//using SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig);
+// Configure speech synthesis
+speechConfig.SpeechSynthesisVoiceName = "en-GB-George"; // add this
+using SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig);
 
             // Synthesize spoken output
+// Synthesize spoken output
+// SpeechSynthesisResult speak = await speechSynthesizer.SpeakTextAsync(responseText);
+// if (speak.Reason != ResultReason.SynthesizingAudioCompleted)
+// {
+//     Console.WriteLine(speak.Reason);
+// }
 
+// Synthesize spoken output
+string responseSsml = $@"
+    <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'>
+        <voice name='en-GB-Susan'>
+            {responseText}
+            <break strength='weak'/>
+            Time to end this lab!
+        </voice>
+    </speak>";
+SpeechSynthesisResult speak = await speechSynthesizer.SpeakSsmlAsync(responseSsml);
+if (speak.Reason != ResultReason.SynthesizingAudioCompleted)
+{
+    Console.WriteLine(speak.Reason);
+}
 
             // Print the response
             Console.WriteLine(responseText);
